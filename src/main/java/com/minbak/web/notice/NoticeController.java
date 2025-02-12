446fc68b5cab84b496e173faa3a982c5d3d4de1e
박성철 <imf345666@gmail.com>
2025-02-12 13:57:07 +0900

    Notice 공지사항 페이징 폼 완성1

--- src/main/java/com/minbak/web/notice/NoticeController.java
+++ src/main/java/com/minbak/web/notice/NoticeController.java
@@ -32,19 +32,19 @@ public class NoticeController {
     }
 //페이징 기능 추가된 공지사항 목록 조회
     @GetMapping("/list")
-    public String getNoticeList(@RequestParam(defaultValue = "1") int page,Model model){
+    public String getNoticeList(@RequestParam(defaultValue = "1") int page,Model model){ // page 값은 URL에서 가져옴
         int pageSize = 5; // 한페이지 당 갯수 설정
         int offset = (page - 1) * pageSize;
 
-        //페이징 적용된 데이터 가져오는
+        //페이징 적용된 데이터 가져옴? // notices는 컨트롤러 → 서비스로 전달(1-1)
         List<NoticeDto> notices = noticeService.getNoticeList(page,pageSize);
         //전체 페이지 개수 계산 해주는
         int totalPages = noticeService.getTotalPages(pageSize);
 
 
-        model.addAttribute("notices", notices);
-        model.addAttribute("currentPage", page);
-        model.addAttribute("totalPages", totalPages);
+        model.addAttribute("notices", notices); // notices는 컨트롤러 → html로 전달(1-2) 하기도 함
+        model.addAttribute("currentPage", page); // 현재 페이지 번호 전달
+        model.addAttribute("totalPages", totalPages); // 총 페이지 수 전달
 
         return "notice/notice-list";
     }
